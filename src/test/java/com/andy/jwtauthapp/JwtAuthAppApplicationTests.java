46c606264e432108696192ce04b61310dc0bd2b8
Andrej Marčan <yami.yatem@gmail.com>
2020-07-23 18:40:47 +0200

    init

---
+++ src/test/java/com/andy/jwtauthapp/JwtAuthAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andy.jwtauthapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
